b07d98521a5e435050501b06516395eded32375f
adil mae <itsadilhadid@gmail.com>
2023-11-27 20:14:55 +0100

    vercel deployment test !!!

--- src/routes/[activateAccount]/+page.server.js
+++ src/routes/[activateAccount]/+page.server.js
@@ -1,7 +1,7 @@
-import { prisma } from '../lib/server/prisma';
+// import { prisma } from '../lib/server/prisma';
 
-export const load = async () => {
-	return {
-		dbTodos: await prisma.todo.findUnique()
-	};
-};
+// export const load = async () => {
+// 	return {
+// 		dbTodos: await prisma.todo.findUnique()
+// 	};
+// };
