d1b0fa76962decd65cc5303472fbe4fd72da9959
adil mae <itsadilhadid@gmail.com>
2023-12-01 21:00:18 +0100

    putting validation expiry as dead code for now

--- src/routes/confirm/[verificationToken]/+page.server.js
+++ src/routes/confirm/[verificationToken]/+page.server.js
@@ -1,36 +1,70 @@
 // Example of a SvelteKit route for handling confirmation links
 // src/routes/confirm/[token].js
-import { redirect } from '@sveltejs/kit';
-import { prisma } from '../../../lib/server/prisma';
+import { redirect } from "@sveltejs/kit";
+import { prisma } from "../../../lib/server/prisma";
 
-// confirm/[verificationToken].js
+// const validateToken = async verificationToken => {
+//   const user = await prisma.user.findUnique({
+//     where: {
+//       verificationToken
+//     }
+//   });
 
-export const load = async (context) => {
-	const { params } = context;
-	const verificationToken = params.verificationToken;
+//   if (user && user.verificationToken === verificationToken) {
+//     const createdAt = user.verificationTokenCreatedAt;
+//     const expirationTime = 3 * 60 * 1000; // 3 minutes in milliseconds
+//     const currentTime = new Date();
 
-	console.log('typeof verificationToken -->', typeof verificationToken);
-	console.log('verificationToken -->', verificationToken);
+//     if (currentTime - createdAt > expirationTime) {
+//       // Token has expired
+//       await prisma.user.update({
+//         where: {
+//           id: user.id
+//         },
+//         data: {
+//           verificationToken: null, // or undefined or ''
+//           verificationTokenCreatedAt: null
+//         }
+//       });
 
-	// Verify the token against the database
-	const user = await prisma.user.findUnique({
-		where: { verificationToken }
-	});
+//       // Token is expired
+//       return false;
+//     }
 
-	console.log('hello -->');
+//     // Token is valid
+//     return true;
+//   }
 
-	if (user && !user.verified) {
-		// Update the user's account to mark it as confirmed
-		await prisma.user.update({
-			where: { id: user.id },
-			data: { verified: true }
-		});
+//   // Invalid token
+//   return false;
+// };
 
-		// Redirect the user to a confirmation success page
+export const load = async context => {
+  const { params } = context;
+  const verificationToken = params.verificationToken;
 
-		throw redirect(303, '/confirm/success');
-	} else {
-		// Redirect the user to a confirmation error page
-		throw redirect(303, '/confirm/error');
-	}
+  console.log("typeof verificationToken -->", typeof verificationToken);
+  console.log("verificationToken -->", verificationToken);
+
+  // Verify the token against the database
+  const user = await prisma.user.findUnique({
+    where: { verificationToken }
+  });
+
+  console.log("hello -->");
+
+  if (user && !user.verified) {
+    // Update the user's account to mark it as confirmed
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { verified: true }
+    });
+
+    // Redirect the user to a confirmation success page
+
+    throw redirect(303, "/confirm/success");
+  } else {
+    // Redirect the user to a confirmation error page
+    throw redirect(303, "/confirm/error");
+  }
 };
